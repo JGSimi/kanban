9ea1037381f195e39b44ef53d3b3479aaf55067c
JGSimi <joao02simi@gmail.com>
2024-11-26 18:18:56 -0300

    Refactor index.html layout, enhance board loading in index.js, and update styles in style.css
    - Updated the HTML structure to include a header and main section for better organization.
    - Added a search input and buttons for project management in the header.
    - Implemented a new function in index.js to dynamically load and display user boards.
    - Changed the font in style.css and added new styles for board elements and dropdown functionality.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import user from "./user.js";
+import requests from "./request.js";
 
 async function checkLogin() {
     if (!user.load() || user.Id === null) {
@@ -6,5 +7,33 @@ async function checkLogin() {
     }
 }
 
-checkLogin();
+async function loadBoards() {
+    const boards = await requests.GetBoards(user.Id);
 
+    boards.forEach(board => {
+        const boardElement = document.createElement("div");
+        boardElement.classList.add("board");
+        boardElement.innerHTML = `
+            <div class="board-content">
+                <div class="board-info">
+                    <h2 class="fnt-lg" title="${board.Name}">${board.Name.length > 20 ? board.Name.substring(0,20) + '...' : board.Name}</h2>
+                </div>
+                <div class="board-actions">
+                    <div class="dropdown">
+                        <button class="btn p-sm border-sm">⋮</button>
+                        <div class="dropdown-content p-sm flex-column gap-sm">
+                            <button class="btn btn-primary p-sm border-sm">✐ Editar</button>
+                            <button class="btn btn-primary p-sm border-sm">🗑️ Excluir</button>
+                            <button class="btn btn-primary p-sm border-sm">📋 Duplicar</button>
+                            <button class="btn btn-primary p-sm border-sm">⭐ Favoritar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        document.getElementById("boards").appendChild(boardElement);
+    });
+}
+
+checkLogin();
+loadBoards();
